2c10b2d6e7879fcb47ac85c9b993f475774cd05f
liyang <7325560@qq.com>
2021-05-12 00:05:25 +0800

    fix git-gutter set fringe-mode 4 issues, use default 8 intead

--- config.el
+++ config.el
@@ -54,6 +54,9 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+(after! git-gutter-fringe
+  (if (fboundp 'fringe-mode) (fringe-mode nil)))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
